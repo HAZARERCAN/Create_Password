742f8a2de311fe8d8f879fa12b9841128d126ed0
İzzet Hazar ERCAN <126699095+HAZARERCAN@users.noreply.github.com>
2023-07-16 20:51:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+def generate_password(lenght,level,output=[]):
+    chars = string.ascii_letters
+    if level > 1:
+        chars = "{}{}".format(chars, string.digits)
+    if level > 2:
+        chars = "{}{}".format(chars, string.punctuation)
+
+    for i in range(lenght):
+        output.append(random.choice(chars))
+
+    return "".join(output)
+
+print(("-"*30 + "\n Password Generator\n") + ("-"*30))
+
+password_lenght = int(input("Length:"))
+password_level = int(input("Level:"))
+
+password = generate_password(password_lenght,password_level)
+print("\nYour Password is: {}".format(password))
+
+
+
+
+
+
+
+
+
